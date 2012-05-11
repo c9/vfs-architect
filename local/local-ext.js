89bd18c02fe5c36a5baae8c86792b2bf19e332ad
Fabian Jakobs <fabian.jakobs@web.de>
2012-05-11 14:20:55 +0200

    add "child" plugin

--- local/local-ext.js
+++ local/local-ext.js
@@ -2,8 +2,6 @@ var assert = require("assert");
 
 module.exports = function setup(options, imports, register) {
 
-    assert.equal(typeof options.uid, "number", "option 'uid' is required");
-    assert.equal(typeof options.gid, "number", "option 'gid' is required");
     assert(options.root, "option 'root' is required");
 
     register(null, {
